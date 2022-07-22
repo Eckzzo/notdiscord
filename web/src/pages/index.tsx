6331862b06570d0ab5c6d799178bf84c46c9d2ca
Eckzzo <ivan.levenhagen@outlook.com>
2022-07-22 14:46:41 -0300

    index

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,9 +1,35 @@
+import { Button } from '@ui/Button';
+import { Container } from '@ui/Container';
+import { Flex } from '@ui/Flex';
+import { Heading } from '@ui/Heading';
+import { Panel } from '@ui/Panel';
+import { Text } from '@ui/Text';
+import { AuthLayout } from 'components/Layout/AuthLayout';
 import { NextPage } from 'next';
+import NextLink from 'next/link';
 
 interface HomeProps {}
 
 const Home: NextPage<HomeProps> = () => {
-	return <h1>NotDiscord!</h1>;
+	return (
+		<AuthLayout>
+			<Container>
+				<Panel css={{ p: '$6', borderRadius: '$4' }}>
+					<Flex direction="column" justify="center" align="center" gap={3}>
+						<Heading variant="h5">Welcome to NotDiscord!</Heading>
+						<NextLink href="/auth/signup">
+							<Button isFullWidth>Sign Up</Button>
+						</NextLink>
+						<NextLink href="/auth/signin">
+							<Button variant="tertiary" isFullWidth>
+								Sign In
+							</Button>
+						</NextLink>
+					</Flex>
+				</Panel>
+			</Container>
+		</AuthLayout>
+	);
 };
 
 export default Home;
